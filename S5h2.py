b1d44b60202e4db8b97c6da3e6f4bddc06f1442f
Parisang <89538490+Parisang@users.noreply.github.com>
2021-09-15 21:32:18 +0430

    Add files via upload

---
+++ S5h2.py
@@ -0,0 +1,21 @@
+#parisa najari ghahremani thursday 14-18 class
+#barnameiy ke 3 ta list ra dar ham mirizad  va az akhar be
+#aval yeki dar mion chap mikonad
+
+list1=[1,2,3]
+list2=[4,5,6]
+list3=[7,8,9]
+list1.extend(list2+list3)
+print(list1[-1: :-2])
+
+
+
+
+
+#ravesh dovom
+list1=[1,2,3]
+list2=[4,5,6]
+list3=[7,8,9]
+list1.extend(list2+list3)
+list1.reverse()
+print(list1[ : :-2])
